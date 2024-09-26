1e0861b707f7df535e8945363720ab213199831c
Gabrielc1925 <gabrielc1925@gmail.com>
2024-09-26 09:37:37 -0600

    Added Readme for several files.  moved resources to two files

--- src/lib/components/information/index.ts
+++ src/lib/components/information/index.ts
@@ -1,3 +1,5 @@
+// This file is used to store the masjid information that will be used in the website.  It should be updated with the correct masjid information in order to make the changes across the website.
+
 interface Masjid {
   name: string;
   address: string;
