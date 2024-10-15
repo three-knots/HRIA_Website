762114fb50a51022f071c4255f337e5ce3e8a14a
Gabrielc1925 <gabrielc1925@gmail.com>
2024-10-15 10:36:53 -0600

    added footer, more pages. Need to add social media svg to footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div
+	class="hero min-h-screen"
+	style="background-image: url(https://img1.wsimg.com/isteam/stock/15760);"
+>
+	<div class="hero-overlay bg-opacity-60"></div>
+	<div class="hero-content text-neutral-content text-center">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Welcome to Highlands Ranch Islamic Association</h1>
+			<p class="mb-5">
+				Serving the Islamic Community of Highlands Ranch and the surrounding area. We are dedicated
+				to supporting the needs of the community and providing a place of education and worship.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
