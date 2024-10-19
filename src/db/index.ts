0abebcc53513b7956dbd5630b3b32d5cfc90f04a
Gabrielc1925 <gabrielc1925@gmail.com>
2024-10-19 10:51:19 -0600

    removed drizzle

--- src/db/index.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from 'drizzle-orm';
-
-async function main() {
-	const db = drizzle('postgres-js', process.env.DATABASE_URL);
-}
-
-main();
--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+import { createClient } from "@supabase/supabase-js";
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from "$env/static/public"
+
+const supabaseUrl = PUBLIC_SUPABASE_URL;
+const supabaseKey = PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+        
