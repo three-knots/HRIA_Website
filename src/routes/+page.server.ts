1a7a7af4e8b15cd71c6e3d265d6e34f7c2b87062
Gabrielc1925 <gabrielc1925@gmail.com>
2024-10-29 23:42:03 -0600

    added announcements to main page and fixed announcements page

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,21 @@
-import { supabase } from '$lib/supabaseClient';
+import type { PageServerLoad } from "./$types";
+import { supabase } from "$lib/supabaseClient";
 
-export async function load() {
-	const { data } = await supabase.from('countries').select();
+export const load: PageServerLoad = async () => {
+	const { data, error } = await supabase
+		.from("Announcements")
+		.select("*")
+		.order("created_at", { ascending: false })
+		.limit(3);
+
+	if (error) {
+		console.error("Error fetching announcements:", error);
+		return {
+			announcements: [],
+		};
+	}
+
 	return {
-		countries: data ?? []
+		announcements: data ?? [],
 	};
-}
+};
