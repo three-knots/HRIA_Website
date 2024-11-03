e579bf471e7a3b6d070e5a3f332c1905ae9c32c7
Gabrielc1925 <gabrielc1925@gmail.com>
2024-11-03 11:46:18 -0700

    removed old formatting on auth page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,12 +1,4 @@
 <script lang="ts">
-    import Card from '$lib/components/ui/card';
 </script>
 
-<h1>Prayer Times</h1>
-
-<Card>
-    <Card.Header>
-        <Card.Title>Prayer Times</Card.Title>
-        <Card.Description>View the prayer times for the day.</Card.Description>
-    </Card.Header>
-</Card>
+<h1>Auth</h1>
