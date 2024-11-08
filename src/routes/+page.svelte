6d370ccdc8f300de6694546db37331485f353907
Gabrielc1925 <gabrielc1925@gmail.com>
2024-11-08 08:21:05 -0700

    finished main page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,33 +20,95 @@
 	</div>
 </div>
 
-<Layout>
+<Layout breakpoint="md">
 	<header slot="header">
+		<div class="divider divider-secondary"></div>
+		<div class="card">
+			<div class="card m-auto w-96">
+				<div class="m-auto w-96 overflow-x-auto">
+					<table class="table table-zebra">
+						<!-- head -->
+						<thead>
+							<tr>
+								<th>Prayer</th>
+								<th>Iqama Time</th>
+							</tr>
+						</thead>
+						<tbody>
+							<!-- row 1 -->
+							<tr>
+								<td>Fajr</td>
+								<td
+									>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'fajr')
+										?.iqama_time}</td
+								>
+							</tr>
+							<!-- row 2 -->
+							<tr>
+								<td>Dhuhr</td>
+								<td
+									>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'dhuhr')
+										?.iqama_time}</td
+								>
+							</tr>
+							<!-- row 3 -->
+							<tr>
+								<td>Asr</td>
+								<td
+									>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'asr')?.iqama_time}</td
+								>
+							</tr>
+							<tr>
+								<td>Maghrib</td>
+								<td
+									>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'maghrib')?.diff_time} minutes
+									after athan</td
+								>
+							</tr>
+							<tr>
+								<td>Isha</td>
+								<td
+									>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'isha')
+										?.iqama_time}</td
+								>
+							</tr>
+						</tbody>
+					</table>
+					<button class="btn btn-accent m-2">
+						<a href="/prayer-times">Prayer Times Page</a>
+					</button>
+				</div>
+			</div>
+		</div>
 		<div class="divider divider-secondary"></div>
 	</header>
 	<section slot="side1">
-		<h2>Announcements</h2>
-		<!-- This automatically adds a new collapsible section for each announcement every time a new announcement is added to the database under the Announcements table -->
+		<h2 class="text-center">Upcoming Events</h2>
+		<!-- This automatically adds a new collapsible section for each event every time a new event is added to the database under the Events table -->
 		<div id="group-focus" class="group-focus group">
-			{#each data.announcements as announcement}
+			{#each data.events as event}
 				<div
 					tabindex="-1"
 					class="collapse bg-warning text-primary-content focus:bg-secondary focus:text-secondary-content"
 				>
-					<div class="collapse-title">{announcement.announcement_subject}</div>
+					<div class="collapse-title">
+						{new Date(event.event_date).toLocaleDateString()}
+						<p class="font-semibold">{event.event_title}</p>
+					</div>
 					<div class="collapse-content">
-						<p>{announcement.announcement_body}</p>
+						<p>{event.event_information}</p>
 					</div>
 				</div>
 				<div class="divider divider-secondary"></div>
 			{/each}
 		</div>
+
 		<div>
-			<a href="/events/announcements" class="btn btn-accent">Announcements Page</a>
+			<a href="/events" class="btn btn-accent">Events Page</a>
 		</div>
 	</section>
 
-	<main slot="main">
+	<main slot="main" style="w-96;">
 		<div class="card m-2 bg-primary text-primary-content">
 			<div class="card-body">
 				<h2 class="card-title">Community</h2>
@@ -91,81 +153,28 @@
 		</div>
 	</main>
 	<section slot="side2">
-		<div class="overflow-x-auto">
-			<table class="table table-zebra">
-				<!-- head -->
-				<thead>
-					<tr>
-						<th>Prayer</th>
-						<th>Iqama Time</th>
-					</tr>
-				</thead>
-				<tbody>
-					<!-- row 1 -->
-					<tr>
-						<td>Fajr</td>
-						<td>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'fajr')?.iqama_time}</td>
-					</tr>
-					<!-- row 2 -->
-					<tr>
-						<td>Dhuhr</td>
-						<td>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'dhuhr')?.iqama_time}</td>
-					</tr>
-					<!-- row 3 -->
-					<tr>
-						<td>Asr</td>
-						<td>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'asr')?.iqama_time}</td>
-					</tr>
-					<tr>
-						<td>Maghrib</td>
-						<td
-							>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'maghrib')?.diff_time} minutes
-							after athan</td
-						>
-					</tr>
-					<tr>
-						<td>Isha</td>
-						<td>{data.iqama.find((iq) => iq.prayer_name.toLowerCase() === 'isha')?.iqama_time}</td>
-					</tr>
-				</tbody>
-			</table>
-			<button class="btn btn-accent m-2">
-				<a href="/prayer-times">Prayer Times Page</a>
-			</button>
-		</div>
-		<div class="divider divider-secondary"></div>
-
-		<h2>Events</h2>
-		<!-- This automatically adds a new collapsible section for each event every time a new event is added to the database under the Events table -->
+		<h2 class="text-center">Announcements</h2>
+		<!-- This automatically adds a new collapsible section for each announcement every time a new announcement is added to the database under the Announcements table -->
 		<div id="group-focus" class="group-focus group">
-			{#each data.events as event}
+			{#each data.announcements as announcement}
 				<div
 					tabindex="-1"
 					class="collapse bg-warning text-primary-content focus:bg-secondary focus:text-secondary-content"
 				>
-					<div class="collapse-title">
-						{new Date(event.event_date).toLocaleDateString()}
-						<p class="font-semibold">{event.event_title}</p>
-					</div>
+					<div class="collapse-title">{announcement.announcement_subject}</div>
 					<div class="collapse-content">
-						<p>{event.event_information}</p>
+						<p>{announcement.announcement_body}</p>
 					</div>
 				</div>
 				<div class="divider divider-secondary"></div>
 			{/each}
 		</div>
-
 		<div>
-			<a href="/events" class="btn btn-accent">Events Page</a>
+			<a href="/events/announcements" class=" btn btn-accent">Announcements Page</a>
 		</div>
 	</section>
 	<footer slot="footer">My Footer</footer>
 </Layout>
 
-console.log(data.events);
-
 <style>
-	:global(.svelte-fluid-layout-large main) {
-		width: 66%;
-	}
 </style>
